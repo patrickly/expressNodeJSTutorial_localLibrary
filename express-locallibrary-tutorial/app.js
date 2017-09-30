e239c9b4c2b0ae6ee490cc4a6e30a43b39e535e1
patrickly <patrickkimly@gmail.com>
2017-09-30 15:06:58 -0700

    removed wiki.js, added route-handler callback functions

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var wiki = require('./routes/wiki');
 
 
 var app = express();
@@ -38,8 +37,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/wiki', wiki);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
